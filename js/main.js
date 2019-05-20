b0929bbf8ec67058d9f0bc30f743faa57ec4de38
aloksoni11 <sonialok5777@gmail.com>
2019-05-20 13:49:18 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function borderchange() {
+    var item = document.getElementById("bordericon");
+    item.classList.toggle("border-green");
+}
+
+function Gstart() {
+    var G = document.getElementById("Gstart");
+    G.classList.toggle("show");
+}
